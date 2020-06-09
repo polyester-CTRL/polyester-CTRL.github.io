ccb57625830bffca51173be5dfa851b71315a42e
polyester <56536708+polyester-CTRL@users.noreply.github.com>
2020-06-10 03:16:09 +0900

    Add files via upload
    yarn

---
+++ docs/precache-manifest.5ac8d879526efde0eb1859f35a4b6cb1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7468650cab85908c5e98",
+    "url": "/vue-tutorial-pwa/css/app.b533f821.css"
+  },
+  {
+    "revision": "fe852ec2e811db532bfd",
+    "url": "/vue-tutorial-pwa/css/chunk-vendors.2ac5db4b.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/vue-tutorial-pwa/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/vue-tutorial-pwa/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "d92cfaebf67cb61b99402baee68b731e",
+    "url": "/vue-tutorial-pwa/index.html"
+  },
+  {
+    "revision": "ec1166d6fb0f52f99a7b",
+    "url": "/vue-tutorial-pwa/js/about.c52e5981.js"
+  },
+  {
+    "revision": "7468650cab85908c5e98",
+    "url": "/vue-tutorial-pwa/js/app.0d1c082f.js"
+  },
+  {
+    "revision": "fe852ec2e811db532bfd",
+    "url": "/vue-tutorial-pwa/js/chunk-vendors.764d5bce.js"
+  },
+  {
+    "revision": "248764d12875a71ef3ae4ea0a2d9070f",
+    "url": "/vue-tutorial-pwa/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vue-tutorial-pwa/robots.txt"
+  }
+]);
