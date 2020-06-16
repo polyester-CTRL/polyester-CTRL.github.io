54f7237b2ce9f628202c09e77a4dc43263be442f
polyester_ctrl <shunfreed2016@gmail.com>
2020-06-17 00:55:52 +0900

    fix about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="about">
-    <h1>準備中です。しばらくお待ちください。</h1>
+    <h1>インストール方法</h1>
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="PC" name="first">PC
+        <ol class="pp">
+          <li>ブラウザを最新版に更新してください</li>
+          <li>アドレスバーの右にある+アイコンをクリックしてください</li>
+        </ol>
+      </el-tab-pane>
+      <el-tab-pane label="Android" name="second">Android</el-tab-pane>
+      <el-tab-pane label="iOS" name="third">iOS</el-tab-pane>
+    </el-tabs>
+    <h2>このアプリについて</h2>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      activeName: 'first'
+    }
+  },
+  methods: {
+  }
+}
+</script>
+<style scoped>
+.el-tabs{
+  margin-left: 25%;
+  margin-right: 25%;
+  width: 50%;
+}
+.pp{
+  width: 300px;
+}
+
+</style>
